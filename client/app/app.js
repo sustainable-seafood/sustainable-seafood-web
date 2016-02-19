daea6798e99783d2f9fd3b302cd0dc16d47596e1
Carson Trent Long <ctlong.970@gmail.com>
2016-02-19 21:12:38 +0800

    updated homepage style

--- client/app/app.js
+++ client/app/app.js
@@ -19,4 +19,4 @@ angular.module('sustainableSeafoodWebApp', [
     $rootScope.$on('$locationChangeSuccess', function() {
       $anchorScroll();
     });
-  })
+  });
--- client/app/directives/footer/footer.directive.js
+++ client/app/directives/footer/footer.directive.js
@@ -3,7 +3,7 @@
 angular.module('sustainableSeafoodWebApp')
   .directive('footer', function() {
     return {
-      templateUrl: 'components/footer/footer.html',
+      templateUrl: 'directives/footer/footer.html',
       restrict: 'E',
       link: function(scope, element) {
         element.addClass('footer');
