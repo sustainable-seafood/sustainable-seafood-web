ef8a62463f7bce349eac64776fbdc7ea2a16e251
Carson Trent Long <ctlong.970@gmail.com>
2016-02-23 16:12:48 +0800

    styled seafood choice pages

--- client/app/routes/seafood/seafood.controller.js
+++ client/app/routes/seafood/seafood.controller.js
@@ -8,4 +8,16 @@ angular.module('sustainableSeafoodWebApp')
       var vm = this;
 
       vm.switch = $state.is('seafood.choice')
+
+      vm.moveBack = function() {
+        if(vm.switch) {
+          $state.go('home');
+        } else {
+          $state.go('seafood.choice');
+        }
+      }
+
+      vm.moveForward = function() {
+        $state.go('seafood.comparison');
+      }
     };
